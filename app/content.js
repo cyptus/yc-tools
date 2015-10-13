cf89f6fdacba51235d9f12a192f9016b5a9c0bc8
cyptus <cyptus@gmail.com>
2015-10-13 11:15:08 +0200

    new structure, added demo screenshot

--- app/content.js
+++ app/content.js
@@ -2,6 +2,7 @@ chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
     if (msg.text && (msg.text == "report_back")) {
         var result = "<style>font-family: Verdana</style>";
         var questions = document.querySelectorAll(".q");
+		
         [].forEach.call(questions, function(q) {
           var qLabel = q.querySelector("label");
           var qVal = document.getElementById(qLabel.htmlFor);
@@ -9,7 +10,9 @@ chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
             result += "<strong># " + qLabel.innerText + "</strong>" + "<br/>" + qVal.value + "<br /><br />";
           }
         });
-        
+		
+        result += "<script>window.print();</script>";
+		
         var tab = window.open();
         tab.document.write(result);
     }
--- readme.md
+++ readme.md
@@ -1,2 +1,5 @@
 # YC Application Print
-chrome plugin that dumps the YC application for printing and correction on paper
+chrome plugin that extracts the YC application for printing and correction on paper
+
+## Extracted demo application)
+![extracted demo application](demo.png?raw=true "extracted demo application")
